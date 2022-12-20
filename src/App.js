8e36339e0903ba334fa919949b01ade32c5be815
leondoes <50689177+leondoes@users.noreply.github.com>
2022-12-20 00:32:07 -0500

    :lipstick: properly implanmented font (#4)

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import "./styles.css";
 import React from "react";
 import PageLayout from "./layout/pageLayout";
 
--- src/components/navbar/styled.js
+++ src/components/navbar/styled.js
@@ -3,6 +3,9 @@ import styled from "styled-components";
 export const navBarHeight = 90;
 
 export const NavbarStyled = styled.div`
+font-family: 'Work Sans';
+font-weight: 900;
+font-size: 80px;
   position: sticky;
   top: 0;
   flex-direction: column;
@@ -15,10 +18,6 @@ export const NavbarStyled = styled.div`
   border: 0;
   border-radius: 3px;
   padding-left: 2%;
-  margin-top: 2%;
-    box-shadow: 0 0 50px black;
-  font-size: 40px;
-  display: flex;
   height: ${navBarHeight - 1 + "px"}; /* subtract 1 for border*/
   transition: height 0.4s ease 0s;
 `;
