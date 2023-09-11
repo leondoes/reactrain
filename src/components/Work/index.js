7ef7e599bf922ac6a6c7b311aa095cb143305255
Leon Dobrescu <50689177+leondoes@users.noreply.github.com>
2023-09-11 04:09:31 -0400

    :lipstick: Work page style fitting rest of website (#22)

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -1,27 +1,42 @@
 import React from "react";
 import PageLayout from "../../layout/PageLayout";
-import { Card, Deck, Title, Subtitle, Copy } from "./styled";
+import { PageContainer, Card, Deck, Title, Subtitle, Copy } from "./styled";
 
 const Work = () => (
   <PageLayout>
-    <Deck>
-      <Card>
-        <div><Title>Hello2</Title><Copy>
-           Lorem Ipsum is simply dummy text of the printing and
-          typesetting industry. Lorem Ipsum has been the industry's standard
-          dummy text ever since the 1500s, when an unknown printer took a galley
-          of type and scrambled it to make a type specimen book. It has survived
-          not only five centuries, but also the leap into electronic
-          typesetting, remaining essentially unchanged. It was popularised in
-          the 1960s with the release of Letraset sheets containing Lorem Ipsum
-          passages, and more recently with desktop publishing software like
-          Aldus PageMaker including versions of Lorem Ipsum.</Copy>
-        </div>
-      </Card>
-      <Card>
-        <div>Hello</div>
-      </Card>
-    </Deck>
+    <PageContainer>
+      <Title>A Showcase of My Web Dev Projects</Title>
+      <Copy>
+        Welcome to my portfolio examples section, where I unveil my prowess as a
+        React developer. These projects are a testament to my commitment to
+        creating exceptional web applications that blend functionality with
+        aesthetics. Each example embodies a unique challenge I've conquered,
+        revealing my knack for transforming concepts into captivating digital
+        solutions. Dive in and be inspired!
+      </Copy>
+      <Deck>
+        <Card>
+          <div>
+            <Subtitle>Hello2</Subtitle>
+            <Copy>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </Copy>
+          </div>
+        </Card>
+        <Card>
+          <Copy>Hello</Copy>
+        </Card>
+      </Deck>
+    </PageContainer>
   </PageLayout>
 );
 
--- src/components/Work/styled.js
+++ src/components/Work/styled.js
@@ -1,46 +1,47 @@
 import styled from "styled-components";
 import { themeColors, themeFonts, mediaQuery } from "../../common/theme";
 
-
-export const Title = styled.div`
-font-weight: 900;
-font-size: 70px;
-padding-bottom: 30vh;
-padding-top: 20vh;
+export const PageContainer = styled.div `
+font-family: ${themeFonts.brandFont};
+display: flex;
+flex-direction:column;
+${mediaQuery.down.tablet} {
+    padding: 0px 20px 0px 20px;
+  }
 `;
 
-export const Subtitle = styled.div `
-font-weight: 700;
-font-size: 50px;
-padding-bottom: 10vh;
-`
+export const Title = styled.div`
+  font-family: ${themeFonts.brandFont};
+  font-weight: 900;
+  font-size: 70px;
+  padding-bottom: 20vh;
+  padding-top: 20vh;
+`;
+
+export const Subtitle = styled.div`
+  font-family: ${themeFonts.brandFont};
+  font-weight: 700;
+  font-size: 50px;
+  padding-bottom: 10vh;
+`;
 
 export const Copy = styled.div`
-font-family: ${themeFonts.bodyFont};
-display: flex;
-flex-direction: column;
-font-size: 24px;
-gap: 20px;
+  font-family: ${themeFonts.bodyFont};
+  display: flex;
+  flex-direction: column;
+  font-size: 24px;
 `;
 
 export const Deck = styled.div`
-  font-family: ${themeFonts.brandFont};
-  margin: auto;
-  display: flex;
-  flex-direction: column;
+  color: ${themeColors.font};
+  font-family: ${themeFonts.bodyFont};  
 `;
 
 export const Card = styled.div`
   box-shadow: 0 10px 4px -4px #575a5e;
-  padding: 10px;
   border-radius: 10px;
   background-color: #222823;
   margin-bottom: 100px;
   margin-top: 100px;
-
-  ${mediaQuery.down.tablet} {
-
-  }
-
-
+  padding: 10px;
 `;
