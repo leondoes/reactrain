58aaaca2ec3e453ddd081139535307af27d14983
Leon Dobrescu <50689177+leondoes@users.noreply.github.com>
2023-10-02 03:47:29 -0400

    :truck:Changed from BrowserRouter to HashRouter to possibly fix the 404 issues. (#50)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import { StrictMode } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import App from './App';
 import About from './components/About';
@@ -10,16 +10,13 @@ import Contact from './components/Contact';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <Routes>
-    <Route path="/" element={<App />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/work" element={<Work/>}/>
-    <Route path="/contact"element={<Contact />} />
-    </Routes>
-    </BrowserRouter>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/work" element={<Work />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </HashRouter>
   </StrictMode>
 );
-
-
-
