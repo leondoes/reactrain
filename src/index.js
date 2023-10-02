40d17b5dd14bd3a12bfc3a5be0c24141a513df05
Leon Dobrescu <50689177+leondoes@users.noreply.github.com>
2023-10-02 04:07:37 -0400

    :truck:Second attempt to fix router to fix 404 issue (#51)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import { StrictMode } from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from './App';
 import About from './components/About';
@@ -10,13 +10,16 @@ import Contact from './components/Contact';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/work" element={<Work />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </HashRouter>
+    <BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
+    <Routes>
+    <Route path="" element={<App />} />
+    <Route path="about" element={<About />} />
+    <Route path="work" element={<Work/>}/>
+    <Route path="contact"element={<Contact />} />
+    </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
+
+
+
