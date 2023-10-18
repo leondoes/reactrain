b5df8e3a7133745dcdca851e9dd1ba965ccb8363
leondoes <leon.dobrescu@gmail.com>
2023-10-18 00:58:54 -0400

    :bento: Added weather icons and connected them to the API

--- src/components/WeatherDisplay/index.js
+++ src/components/WeatherDisplay/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import WeatherIcon from '../WeatherIcons';
 
 const WeatherDisplay = ({ weatherData }) => {
   if (!weatherData) return null;
@@ -6,9 +7,11 @@ const WeatherDisplay = ({ weatherData }) => {
 
   return (
     <div>
-      <h2>Weather for {weatherData.name}</h2>
+      <h2>Weather for {weatherData.name} {weatherData.sys.country}</h2>
       <p>Temperature: {Math.round(weatherData.main.temp)}°C</p>
+      <p>Feels Like: {Math.round(weatherData.main.temp)}°C</p>
       <p>Weather: {weatherData.weather[0].description}</p>
+      <WeatherIcon iconCode={weatherData.weather[0].icon} />
     </div>
   );
 };
