560a5a64da3567d9518b0885e48bed5350b31e7a
leondoes <leon.dobrescu@gmail.com>
2023-10-20 04:29:14 -0400

    :sparkle: Added daily high and daily low, also made search display first item automatically, and pressing enter searches instead of just pressing the button

--- src/components/ReactRain/index.js
+++ src/components/ReactRain/index.js
@@ -1,61 +1,101 @@
-import React, { useState, useEffect } from 'react';
-import SearchBar from '../SearchBar'; // Import SearchBar component
-import WeatherDisplay from '../WeatherDisplay'; // Import WeatherDisplay component
+import React, { useState, useEffect } from "react";
+import SearchBar from "../SearchBar";
+import WeatherDisplay from "../WeatherDisplay";
 
 const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
-const geoApiUrl = 'https://api.openweathermap.org/geo/1.0/direct';
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather";
+const geoApiUrl = "https://api.openweathermap.org/geo/1.0/direct";
+const forecastApiUrl = "https://api.openweathermap.org/data/2.5/forecast";
 
 const ReactRain = () => {
   const [weatherData, setWeatherData] = useState(null);
   const [cityNotFound, setCityNotFound] = useState(false);
   const [matchingCities, setMatchingCities] = useState([]);
+  const [dailyHigh, setDailyHigh] = useState(null);
+  const [dailyLow, setDailyLow] = useState(null);
 
   const fetchWeatherByLatLng = async (lat, lon) => {
     try {
-      const response = await fetch(`${apiUrl}?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`);
+      const response = await fetch(
+        `${apiUrl}?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
+      );
+
       if (response.ok) {
         const data = await response.json();
         setWeatherData(data);
-        setCityNotFound(false); // Reset city not found error if data is fetched.
+        setCityNotFound(false);
       } else {
-        setCityNotFound(true); // Set city not found error to true.
-        console.error('Failed to fetch weather data');
+        setCityNotFound(true);
+        console.error("Failed to fetch weather data");
       }
     } catch (error) {
-      console.error('Error while fetching weather data', error);
+      console.error("Error while fetching weather data", error);
+    }
+  };
+
+  const fetchForecastData = async (lat, lon) => {
+    try {
+      const response = await fetch(
+        `${forecastApiUrl}?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        const dailyTemperatures = data.list
+          .slice(0, 8)
+          .map((item) => item.main.temp);
+        const dailyHighTemp = Math.round(Math.max(...dailyTemperatures));
+        const dailyLowTemp = Math.round(Math.min(...dailyTemperatures));
+
+        setDailyHigh(dailyHighTemp);
+        setDailyLow(dailyLowTemp);
+      } else {
+        console.error("Failed to fetch forecast data");
+      }
+    } catch (error) {
+      console.error("Error while fetching forecast data", error);
     }
   };
 
   useEffect(() => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        const { latitude, longitude } = position.coords;
+      navigator.geolocation.getCurrentPosition(({ coords }) => {
+        const { latitude, longitude } = coords;
         fetchWeatherByLatLng(latitude, longitude);
+        fetchForecastData(latitude, longitude);
       });
     }
   }, []);
 
-  const handleCitySearch = async (city) => {
-    // Reset cityNotFound state when a new city search is initiated
+  const resetCitySearch = () => {
     setCityNotFound(false);
-  
+    setWeatherData(null);
+  };
+
+  const handleCitySearch = async (city) => {
+    resetCitySearch();
+
     try {
-      const geoResponse = await fetch(`${geoApiUrl}?q=${city}&limit=5&appid=${apiKey}&type=city`);
+      const geoResponse = await fetch(
+        `${geoApiUrl}?q=${city}&limit=5&appid=${apiKey}&type=city`
+      );
+
       if (geoResponse.ok) {
         const geoData = await geoResponse.json();
+
         if (geoData.length > 0) {
           setMatchingCities(geoData);
+          fetchWeatherByLatLng(geoData[0].lat, geoData[0].lon); // Fetch weather for the first city
         } else {
           setCityNotFound(true);
-          console.error('City not found');
+          console.error("City not found");
         }
       } else {
         setCityNotFound(true);
-        console.error('Failed to fetch city data');
+        console.error("Failed to fetch city data");
       }
     } catch (error) {
-      console.error('Error while fetching city data', error);
+      console.error("Error while fetching city data", error);
     }
   };
 
@@ -69,12 +109,22 @@ const ReactRain = () => {
         <>
           <ul>
             {matchingCities.map((city, index) => (
-              <li key={index} onClick={() => fetchWeatherByLatLng(city.lat, city.lon)}>
-                {city.name}, {city.state}, {city.country}
+              <li
+                key={index}
+                onClick={() => fetchWeatherByLatLng(city.lat, city.lon)}
+              >
+                {`${city.name}, ${city.state}, ${city.country}`}
               </li>
             ))}
           </ul>
-          {weatherData && <WeatherDisplay weatherData={weatherData} />}
+
+          {weatherData && (
+            <WeatherDisplay
+              weatherData={weatherData}
+              dailyHigh={dailyHigh}
+              dailyLow={dailyLow}
+            />
+          )}
         </>
       )}
     </div>
--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -9,6 +9,13 @@ const SearchBar = ({ onSearch }) => {
     }
   };
 
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      // Check if the Enter key was pressed
+      handleSearch();
+    }
+  };
+
   return (
     <div>
       <input
@@ -16,6 +23,7 @@ const SearchBar = ({ onSearch }) => {
         placeholder="Enter a city"
         value={city}
         onChange={(e) => setCity(e.target.value)}
+        onKeyPress={handleKeyPress} // Add the event listener for Enter key press
       />
       <button onClick={handleSearch}>Search</button>
     </div>
